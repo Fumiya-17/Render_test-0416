c4199f3d45e1f5d817ae21ffa7eb09c895e799c5
Fumiya-17 <ratunil.ralo17@gmail.com>
2026-04-16 03:17:51 +0000

    100000

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World :3"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
